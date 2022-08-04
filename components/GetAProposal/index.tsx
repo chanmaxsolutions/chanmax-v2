3edb9ccd7fa8e2b7ac03ae40b3dee52577e4de99
raghuchanmax <chanmaxsolutions@gmail.com>
2022-08-04 09:55:10 +0530

    commit

--- components/GetAProposal/index.tsx
+++ components/GetAProposal/index.tsx
@@ -1,12 +1,15 @@
 import { Modal } from "antd";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { HiX } from "react-icons/hi";
 import Slide from "./Slide";
 
 export default function GetAProposal() {
     const { asPath, pathname, push, query } = useRouter();
     const [visible, setVisible] = useState(false);
+
+    const Provider = visible ? GoogleReCaptchaProvider : ({}: any) => <></>;
 
     const handleClose = () => {
         if (query.slug) {
@@ -25,19 +28,21 @@ export default function GetAProposal() {
     }, [asPath]);
 
     return (
-        <Modal
-            visible={visible}
-            onCancel={handleClose}
-            closable={false}
-            footer={null}
-            width="95vw"
-            bodyStyle={{ height: "90vh", overflowY: "auto" }}
-            centered
-        >
-            <button onClick={handleClose} className="absolute right-10 top-10 z-10">
-                <HiX size={30} />
-            </button>
-            <Slide />
-        </Modal>
+        <Provider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPCHA_SITE_KEY || ""}>
+            <Modal
+                visible={visible}
+                onCancel={handleClose}
+                closable={false}
+                footer={null}
+                width="95vw"
+                bodyStyle={{ height: "90vh", overflowY: "auto" }}
+                centered
+            >
+                <button onClick={handleClose} className="absolute right-10 top-10 z-10">
+                    <HiX size={30} />
+                </button>
+                <Slide />
+            </Modal>
+        </Provider>
     );
 }
