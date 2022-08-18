6d2b960257c9ff1612b8f37ad7368745dd353021
raghuchanmax <chanmaxsolutions@gmail.com>
2022-08-18 20:35:23 +0530

    add messenger chat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,13 @@ import { useEffect, useState } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoaded(true);
-        }, 10000);
-    }, []);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setIsLoaded(true);
+    //     }, 10000);
+    // }, []);
 
     return (
         <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPCHA_SITE_KEY || ""}>
@@ -37,11 +37,35 @@ function MyApp({ Component, pageProps }: AppProps) {
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
                             gtag('config', 'G-2ZXS5N90MG');
-                        `}
+                            `}
+                    </Script>
+                    <Script strategy="lazyOnload">
+                        {` var chatbox = document.getElementById('fb-customer-chat');
+                        chatbox.setAttribute("page_id", "343512443052636");
+                        chatbox.setAttribute("attribution", "biz_inbox");`}
+                    </Script>
+                    <Script strategy="lazyOnload">
+                        {`window.fbAsyncInit = function() {
+                            FB.init({
+                            xfbml            : true,
+                            version          : 'v14.0'
+                            });
+                        };
+
+                        (function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));`}
                     </Script>
                 </>
             )}
             <GetAProposal />
+            <div id="fb-root"></div>
+
+            <div id="fb-customer-chat" className="fb-customerchat"></div>
             <Component {...pageProps} />
         </GoogleReCaptchaProvider>
     );
