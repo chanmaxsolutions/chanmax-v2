830d7bd95d85c14b720230e5ae14c1da1f40036c
Prashankhan Chandrapalan <chanmaxsolutions@gmail.com>
2022-08-27 13:53:31 +0530

    updatedpixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -60,6 +60,23 @@ function MyApp({ Component, pageProps }: AppProps) {
                             fjs.parentNode.insertBefore(js, fjs);
                         }(document, 'script', 'facebook-jssdk'));`}
                     </Script>
+
+
+                    
+                    <Script>
+                    {`!function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '770761294196632');
+                    fbq('track', 'PageView');`}
+                    </Script>
+                    
+
                 </>
             )}
             <GetAProposal />
