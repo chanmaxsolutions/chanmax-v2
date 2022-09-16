1f0aa2e121b944a968b30f81476abd3f92a0f55f
raghuchanmax <raghuraj@chanmax.io>
2022-09-16 18:46:58 +0530

    commit

--- components/Cards/CareerCard.tsx
+++ components/Cards/CareerCard.tsx
@@ -6,7 +6,7 @@ export default function CareerCard({ title, shortDescription, slug, type, level,
     return (
         <Link href={`/career/${slug.current}`}>
             <a className="rounded-[10px] bg-[#F8F8F8]">
-                <div className="p-[50px] pt-[30px]">
+                <div className="p-3 lg:p-[50px] pt-[30px]">
                     <h2 className="font-AvenirBold text-2xl">{title}</h2>
                     <div className="mt-5 flex items-center gap-4 overflow-y-scroll lg:overflow-hidden">
                         <mark className="rounded-full bg-primary px-4 py-1 text-dark whitespace-nowrap">{category}</mark>
